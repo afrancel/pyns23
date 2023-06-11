02af6396ca404b841e154117f8abfb474e97e299
afrancel <hi@afrancel.com>
2023-06-11 09:44:54 -0400

    update files

--- ejerciciosramdom.py
+++
@@ -1,2 +0,0 @@
-list = ["francel", 163, "testing"]
-print(list[0], list[1])
--- lista-encontrar.py
+++
@@ -1,8 +0,0 @@
-preguntaUsuario = input("Ingrese un ingrediente")
-
-listaIngredientes = ['harina', 'mantequilla', 'leche', 'canela']
-
-if listaIngredientes.count(preguntaUsuario) > 0:
-    print('El dato ingresado' + str(preguntaUsuario) + 'si existe')
-else:
-    print('El dato ingresado' + str(preguntaUsuario) + 'no existe')
